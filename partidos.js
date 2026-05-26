6699f92149d22a007c91997cc17127d57171811e
Juan <jsammtz@gmail.com>
2026-05-25 18:16:59 -0600

    Admin login y ranking tabla

--- partidos.js
+++ partidos.js
@@ -1,11 +1,12 @@
 const partidos = [
 
-/* =========================================
-   GRUPO A
-========================================= */
+/* =========================
+   FASE DE GRUPOS
+========================= */
 
 {
   id:1,
+  fase:"grupos",
   grupo:"A",
   jornada:1,
   local:"México",
@@ -14,6 +15,7 @@ const partidos = [
 
 {
   id:2,
+  fase:"grupos",
   grupo:"A",
   jornada:1,
   local:"República de Corea",
@@ -22,6 +24,7 @@ const partidos = [
 
 {
   id:3,
+  fase:"grupos",
   grupo:"A",
   jornada:2,
   local:"México",
@@ -30,6 +33,7 @@ const partidos = [
 
 {
   id:4,
+  fase:"grupos",
   grupo:"A",
   jornada:2,
   local:"Sudáfrica",
@@ -38,6 +42,7 @@ const partidos = [
 
 {
   id:5,
+  fase:"grupos",
   grupo:"A",
   jornada:3,
   local:"México",
@@ -46,585 +51,72 @@ const partidos = [
 
 {
   id:6,
+  fase:"grupos",
   grupo:"A",
   jornada:3,
   local:"Sudáfrica",
   visitante:"República de Corea"
 },
 
-/* =========================================
-   GRUPO B
-========================================= */
+/* =========================
+   OCTAVOS
+========================= */
 
 {
-  id:7,
-  grupo:"B",
+  id:100,
+  fase:"octavos",
+  grupo:"OCTAVOS",
   jornada:1,
-  local:"Canadá",
-  visitante:"Bosnia y Herzegovina"
+  local:"1A",
+  visitante:"2B"
 },
 
 {
-  id:8,
-  grupo:"B",
+  id:101,
+  fase:"octavos",
+  grupo:"OCTAVOS",
   jornada:1,
-  local:"Catar",
-  visitante:"Suiza"
+  local:"1B",
+  visitante:"2A"
 },
 
-{
-  id:9,
-  grupo:"B",
-  jornada:2,
-  local:"Canadá",
-  visitante:"Catar"
-},
+/* =========================
+   CUARTOS
+========================= */
 
 {
-  id:10,
-  grupo:"B",
-  jornada:2,
-  local:"Bosnia y Herzegovina",
-  visitante:"Suiza"
-},
-
-{
-  id:11,
-  grupo:"B",
-  jornada:3,
-  local:"Canadá",
-  visitante:"Suiza"
-},
-
-{
-  id:12,
-  grupo:"B",
-  jornada:3,
-  local:"Bosnia y Herzegovina",
-  visitante:"Catar"
-},
-
-/* =========================================
-   GRUPO C
-========================================= */
-
-{
-  id:13,
-  grupo:"C",
+  id:200,
+  fase:"cuartos",
+  grupo:"CUARTOS",
   jornada:1,
-  local:"Brasil",
-  visitante:"Marruecos"
+  local:"Ganador O1",
+  visitante:"Ganador O2"
 },
 
+/* =========================
+   SEMIFINAL
+========================= */
+
 {
-  id:14,
-  grupo:"C",
+  id:300,
+  fase:"semifinal",
+  grupo:"SEMIFINAL",
   jornada:1,
-  local:"Haití",
-  visitante:"Escocia"
+  local:"Ganador C1",
+  visitante:"Ganador C2"
 },
 
-{
-  id:15,
-  grupo:"C",
-  jornada:2,
-  local:"Brasil",
-  visitante:"Haití"
-},
+/* =========================
+   FINAL
+========================= */
 
 {
-  id:16,
-  grupo:"C",
-  jornada:2,
-  local:"Marruecos",
-  visitante:"Escocia"
-},
-
-{
-  id:17,
-  grupo:"C",
-  jornada:3,
-  local:"Brasil",
-  visitante:"Escocia"
-},
-
-{
-  id:18,
-  grupo:"C",
-  jornada:3,
-  local:"Marruecos",
-  visitante:"Haití"
-},
-
-/* =========================================
-   GRUPO D
-========================================= */
-
-{
-  id:19,
-  grupo:"D",
+  id:400,
+  fase:"final",
+  grupo:"FINAL",
   jornada:1,
-  local:"EE. UU.",
-  visitante:"Paraguay"
-},
-
-{
-  id:20,
-  grupo:"D",
-  jornada:1,
-  local:"Australia",
-  visitante:"Turquía"
-},
-
-{
-  id:21,
-  grupo:"D",
-  jornada:2,
-  local:"EE. UU.",
-  visitante:"Australia"
-},
-
-{
-  id:22,
-  grupo:"D",
-  jornada:2,
-  local:"Paraguay",
-  visitante:"Turquía"
-},
-
-{
-  id:23,
-  grupo:"D",
-  jornada:3,
-  local:"EE. UU.",
-  visitante:"Turquía"
-},
-
-{
-  id:24,
-  grupo:"D",
-  jornada:3,
-  local:"Paraguay",
-  visitante:"Australia"
-},
-
-/* =========================================
-   GRUPO E
-========================================= */
-
-{
-  id:25,
-  grupo:"E",
-  jornada:1,
-  local:"Alemania",
-  visitante:"Curazao"
-},
-
-{
-  id:26,
-  grupo:"E",
-  jornada:1,
-  local:"Costa de Marfil",
-  visitante:"Ecuador"
-},
-
-{
-  id:27,
-  grupo:"E",
-  jornada:2,
-  local:"Alemania",
-  visitante:"Costa de Marfil"
-},
-
-{
-  id:28,
-  grupo:"E",
-  jornada:2,
-  local:"Curazao",
-  visitante:"Ecuador"
-},
-
-{
-  id:29,
-  grupo:"E",
-  jornada:3,
-  local:"Alemania",
-  visitante:"Ecuador"
-},
-
-{
-  id:30,
-  grupo:"E",
-  jornada:3,
-  local:"Curazao",
-  visitante:"Costa de Marfil"
-},
-
-/* =========================================
-   GRUPO F
-========================================= */
-
-{
-  id:31,
-  grupo:"F",
-  jornada:1,
-  local:"Países Bajos",
-  visitante:"Japón"
-},
-
-{
-  id:32,
-  grupo:"F",
-  jornada:1,
-  local:"Suecia",
-  visitante:"Túnez"
-},
-
-{
-  id:33,
-  grupo:"F",
-  jornada:2,
-  local:"Países Bajos",
-  visitante:"Suecia"
-},
-
-{
-  id:34,
-  grupo:"F",
-  jornada:2,
-  local:"Japón",
-  visitante:"Túnez"
-},
-
-{
-  id:35,
-  grupo:"F",
-  jornada:3,
-  local:"Países Bajos",
-  visitante:"Túnez"
-},
-
-{
-  id:36,
-  grupo:"F",
-  jornada:3,
-  local:"Japón",
-  visitante:"Suecia"
-},
-
-/* =========================================
-   GRUPO G
-========================================= */
-
-{
-  id:37,
-  grupo:"G",
-  jornada:1,
-  local:"Bélgica",
-  visitante:"Egipto"
-},
-
-{
-  id:38,
-  grupo:"G",
-  jornada:1,
-  local:"RI de Irán",
-  visitante:"Nueva Zelanda"
-},
-
-{
-  id:39,
-  grupo:"G",
-  jornada:2,
-  local:"Bélgica",
-  visitante:"RI de Irán"
-},
-
-{
-  id:40,
-  grupo:"G",
-  jornada:2,
-  local:"Egipto",
-  visitante:"Nueva Zelanda"
-},
-
-{
-  id:41,
-  grupo:"G",
-  jornada:3,
-  local:"Bélgica",
-  visitante:"Nueva Zelanda"
-},
-
-{
-  id:42,
-  grupo:"G",
-  jornada:3,
-  local:"Egipto",
-  visitante:"RI de Irán"
-},
-
-/* =========================================
-   GRUPO H
-========================================= */
-
-{
-  id:43,
-  grupo:"H",
-  jornada:1,
-  local:"España",
-  visitante:"Islas de Cabo Verde"
-},
-
-{
-  id:44,
-  grupo:"H",
-  jornada:1,
-  local:"Arabia Saudí",
-  visitante:"Uruguay"
-},
-
-{
-  id:45,
-  grupo:"H",
-  jornada:2,
-  local:"España",
-  visitante:"Arabia Saudí"
-},
-
-{
-  id:46,
-  grupo:"H",
-  jornada:2,
-  local:"Islas de Cabo Verde",
-  visitante:"Uruguay"
-},
-
-{
-  id:47,
-  grupo:"H",
-  jornada:3,
-  local:"España",
-  visitante:"Uruguay"
-},
-
-{
-  id:48,
-  grupo:"H",
-  jornada:3,
-  local:"Islas de Cabo Verde",
-  visitante:"Arabia Saudí"
-},
-
-/* =========================================
-   GRUPO I
-========================================= */
-
-{
-  id:49,
-  grupo:"I",
-  jornada:1,
-  local:"Francia",
-  visitante:"Senegal"
-},
-
-{
-  id:50,
-  grupo:"I",
-  jornada:1,
-  local:"Irak",
-  visitante:"Noruega"
-},
-
-{
-  id:51,
-  grupo:"I",
-  jornada:2,
-  local:"Francia",
-  visitante:"Irak"
-},
-
-{
-  id:52,
-  grupo:"I",
-  jornada:2,
-  local:"Senegal",
-  visitante:"Noruega"
-},
-
-{
-  id:53,
-  grupo:"I",
-  jornada:3,
-  local:"Francia",
-  visitante:"Noruega"
-},
-
-{
-  id:54,
-  grupo:"I",
-  jornada:3,
-  local:"Senegal",
-  visitante:"Irak"
-},
-
-/* =========================================
-   GRUPO J
-========================================= */
-
-{
-  id:55,
-  grupo:"J",
-  jornada:1,
-  local:"Argentina",
-  visitante:"Argelia"
-},
-
-{
-  id:56,
-  grupo:"J",
-  jornada:1,
-  local:"Austria",
-  visitante:"Jordania"
-},
-
-{
-  id:57,
-  grupo:"J",
-  jornada:2,
-  local:"Argentina",
-  visitante:"Austria"
-},
-
-{
-  id:58,
-  grupo:"J",
-  jornada:2,
-  local:"Argelia",
-  visitante:"Jordania"
-},
-
-{
-  id:59,
-  grupo:"J",
-  jornada:3,
-  local:"Argentina",
-  visitante:"Jordania"
-},
-
-{
-  id:60,
-  grupo:"J",
-  jornada:3,
-  local:"Argelia",
-  visitante:"Austria"
-},
-
-/* =========================================
-   GRUPO K
-========================================= */
-
-{
-  id:61,
-  grupo:"K",
-  jornada:1,
-  local:"Portugal",
-  visitante:"RD Congo"
-},
-
-{
-  id:62,
-  grupo:"K",
-  jornada:1,
-  local:"Uzbekistán",
-  visitante:"Colombia"
-},
-
-{
-  id:63,
-  grupo:"K",
-  jornada:2,
-  local:"Portugal",
-  visitante:"Uzbekistán"
-},
-
-{
-  id:64,
-  grupo:"K",
-  jornada:2,
-  local:"RD Congo",
-  visitante:"Colombia"
-},
-
-{
-  id:65,
-  grupo:"K",
-  jornada:3,
-  local:"Portugal",
-  visitante:"Colombia"
-},
-
-{
-  id:66,
-  grupo:"K",
-  jornada:3,
-  local:"RD Congo",
-  visitante:"Uzbekistán"
-},
-
-/* =========================================
-   GRUPO L
-========================================= */
-
-{
-  id:67,
-  grupo:"L",
-  jornada:1,
-  local:"Inglaterra",
-  visitante:"Croacia"
-},
-
-{
-  id:68,
-  grupo:"L",
-  jornada:1,
-  local:"Ghana",
-  visitante:"Panamá"
-},
-
-{
-  id:69,
-  grupo:"L",
-  jornada:2,
-  local:"Inglaterra",
-  visitante:"Ghana"
-},
-
-{
-  id:70,
-  grupo:"L",
-  jornada:2,
-  local:"Croacia",
-  visitante:"Panamá"
-},
-
-{
-  id:71,
-  grupo:"L",
-  jornada:3,
-  local:"Inglaterra",
-  visitante:"Panamá"
-},
-
-{
-  id:72,
-  grupo:"L",
-  jornada:3,
-  local:"Croacia",
-  visitante:"Ghana"
+  local:"Ganador S1",
+  visitante:"Ganador S2"
 }
 
 ];
-
-
-console.log("🔥 Total partidos:", partidos.length);
